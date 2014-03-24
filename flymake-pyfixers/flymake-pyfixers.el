d22ba02271597fc09caf7279144dd0327d014d31
Christian E. Hopps <chopps@gmail.com>
2014-03-24 06:07:33 -0400

    Add Org mode header

---
+++ flymake-pyfixers/flymake-pyfixers.el
@@ -0,0 +1,162 @@
+;;
+;; March 4 2014, Christian Hopps <chopps@gmail.com>
+;;
+;; Copyright (c) 2014 by Christian E. Hopps.
+;; All rights reserved.
+;;
+(require 'flymake)
+
+;; See if we can load pymacs fixer functions fail silently if we cannot.
+(with-demoted-errors
+  (require 'pymacs)
+  (message "add-directory: %s" (file-name-directory load-file-name))
+  (add-to-list 'pymacs-load-path (file-name-directory load-file-name))
+  (pymacs-load "flymake-pyfixers" "pyfixer:"))
+
+(defvar pyfixer:flymake-fixers
+      '(
+        ("W291" . 'pyfixer:fixer-remove-trailing-ws)
+        ("E221" . 'pyfixer:space-around-op)
+        ("E222" . 'pyfixer:space-around-op)
+        ("E225" . 'pyfixer:space-around-op)
+        ("E226" . 'pyfixer:space-around-op)
+        ("E225" . 'pyfixer:space-around-op)
+        ("E226" . 'pyfixer:space-around-op)
+        ("E227" . 'pyfixer:space-around-op)
+        ("E228" . 'pyfixer:space-around-op)
+        ("E231" . 'pyfixer:comma-space)
+        ("E251" . 'pyfixer:remove-space-around-equals)
+        ("E301" . 'pyfixer:add-blank-line)
+        ("E302" . 'pyfixer:add-blank-line)
+        ("E303" . 'pyfixer:remove-blank-lines)
+        ("E711" . 'pyfixer:equals-none)
+        )
+      "Associative array to locate fixers for pyflake errros, the
+      values functions are quoted to allow pre-referencing")
+
+;; Elisp version of fixers
+(defun pyfixer:space-around-op (errno errinfo)
+  "Fix space around equals warning"
+  (let ((end (line-end-position)))
+    (save-excursion
+      (beginning-of-line)
+      (while (re-search-forward
+              " *\\(!=\\|==?\\|\\*=?\\|\\+=?\\|\\-=?\\|/=?\\|\\^=?\\||=?\\|&=?\\|<<=?\\|>>=?\\|%=?\\) *" end t)
+        (replace-match " \\1 ")
+        (setq end (line-end-position))))))
+
+(defun pyfixer:remove-space-around-equals (errno errinfo)
+  "Fix space around equals warning"
+  (let ((end (line-end-position)))
+    (save-excursion
+      (beginning-of-line)
+      (while (re-search-forward " *= *" end t)
+        (replace-match "=")
+        (setq end (line-end-position))))))
+
+(defun pyfixer:comma-space (errno errinfo)
+  "Fix space around comma warning"
+  (let ((end (line-end-position))
+        newtext)
+    (save-excursion
+      (beginning-of-line)
+      (while (re-search-forward " *, *" end t)
+        (setq newtext (concat (match-string 1) " = " (match-string 2)))
+        (replace-match ", " t)
+        (setq end (line-end-position))))))
+
+(defun pyfixer:equals-none (errno errinfo)
+  "Fix space around equals warning"
+  (let ((end (line-end-position)))
+    (save-excursion
+      (beginning-of-line)
+      (while (re-search-forward "== *None" end t)
+        (replace-match "is None")
+        (setq end (line-end-position)))
+      (while (re-search-forward "!= *None" end t)
+        (replace-match "is not None")
+        (setq end (line-end-position))))))
+
+(defun pyfixer:remove-blank-lines (errno errinfo)
+  "Remove blank line above current line"
+  (let ((beg (point)))
+    (save-excursion
+      ;; move N-1 lines forward
+      (beginning-of-line 0)
+      (forward-line 1)
+      (forward-char -1)
+      (delete-region beg (point)))))
+
+(defun line-no-commentp ()
+  (save-match-data
+    (let* ((start (line-beginning-position))
+           (end (line-end-position))
+           (line (buffer-substring-no-properties start end)))
+      (not (string-match "[:space:]*#.*" line)))))
+
+(defun pyfixer:add-blank-line (errno errinfo)
+  "Add blank line above current line"
+  (save-excursion
+    (let ((lines 0))
+      (if (string-match "expected \\([0-9]+\\) blank lines?, found \\([0-9]+\\)" errinfo)
+          (setq lines (- (string-to-number (match-string 1 errinfo))
+                         (string-to-number (match-string 2 errinfo)))))
+      (beginning-of-line)
+      (previous-line)
+      (while (not (line-no-commentp))
+        (previous-line))
+      (next-line)
+      (newline lines))))
+
+(defun pyfixer:fix-error (errdata)
+  "Fix the given errdata"
+  (if errdata
+      (let (errno fixer)
+        ;; Handle flake8 or pylint messages
+        (if (string-match "\\(^\\([EW][0-9]+\\) \\|\\[\\([EW][0-9]+\\)\\(-.*\\)\\?*\\]\\).*" errdata)
+            (progn
+              (if (not (setq errno (match-string 2 errdata)))
+                  (setq errno (match-string 3 errdata)))
+              (setq fixer (caddr (assoc errno pyfixer:flymake-fixers)))
+              ;; If we don't have and elisp fixer check pymacs fixers
+              (if (and (not fixer) (functionp 'pyfixers-get-fixer-func))
+                  (setq fixer (pyfixers-get-fixer-func errno)))
+              (if fixer
+                  (progn
+                    (funcall fixer errno errdata)
+                    (flymake-start-syntax-check))
+                (message "No pyfixer function for: %s" errno)))))))
+
+(defun pyfixer:fix-current-line ()
+  "Display a fix for the current line"
+  (interactive)
+  (let* ((line-no             (flymake-current-line-no))
+      (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no)))
+      (menu-data           (flymake-make-err-menu-data line-no line-err-info-list))
+      (errlist (caadr menu-data)))
+    (message "Errlist: %s" errlist)
+    (mapcar 'pyfixer:fix-error errlist)))
+
+(defun pyfixer:ignore-error (errdata)
+  "Ignore the given errdata pylint error"
+  (if errdata
+      (let (errno fixer)
+        ;; Handle pylint messages
+        (if (string-match "\\[\\([EW][0-9]+\\)\\(-.*\\)\\?*\\].*" errdata)
+            (progn
+              (setq errno (match-string 1 errdata))
+              (end-of-line)
+              (insert (format "  # pylint: disable=%s" errno)))))))
+
+(defun pyfixer:ignore-current-line ()
+  "Display a fix for the current line"
+  (interactive)
+  (let* ((line-no             (flymake-current-line-no))
+      (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no)))
+      (menu-data           (flymake-make-err-menu-data line-no line-err-info-list))
+      (errlist (caadr menu-data)))
+    (message "Errlist: %s" errlist)
+    (mapcar 'pyfixer:ignore-error errlist)))
+
+
+(provide 'flymake-pyfixers)
---
+++ flymake-pyfixers/flymake-pyfixers.py
@@ -0,0 +1,187 @@
+#
+# February 12 2014, Christian Hopps <chopps@gmail.com>
+#
+# Copyright (c) 2014 by Christian E. Hopps.
+# All rights reserved.
+from __future__ import absolute_import, division, unicode_literals, print_function, nested_scopes
+import re
+from Pymacs import lisp
+interactions = {}
+
+class foo (object):
+
+    def method1 (self):
+        pass
+
+    # foobar
+    def method2 (self, unused):
+        foo = 1
+        foo += 1*2
+        foo -= 1 + 2
+        foo /= 1 - 2
+        foo = 1 / 2
+        foo = 1 % 2
+        foo = 1 ^ 2
+        foo = 1 & 2
+        foo = 1 | 2
+        foo = 1 << 2
+        foo = 1 >> 2
+        if foo is None:
+            pass
+        elif foo is not None:
+            pass
+        pass
+
+    def method3 (self):
+        pass
+
+
+def make_lisp_interactive (func):
+    print("[funcwrapper: {}".format(func.func_name))
+    interactions[func] = ''
+    return func
+
+
+def get_line_string ():
+    start = lisp.line_beginning_position()
+    end = lisp.line_end_position()
+    return start, end, lisp.buffer_substring(start, end)
+
+
+def fixer_remove_space_around_equals (unused_error, unused_errtext):
+    start, end, line = get_line_string()
+    newline = re.sub(r"(\S) *= *(\S)", r"\1=\2", line)
+    if newline != line:
+        lisp.delete_region(start, end)
+        lisp.insert(newline)
+
+
+def fixer_add_one_space_around_equals (unused_error, unused_errtext):
+    start, end, line = get_line_string()
+    newline = re.sub(r"(\S) *= *(\S)", r"\1 = \2", line)
+    if newline != line:
+        lisp.delete_region(start, end)
+        lisp.insert(newline)
+
+
+def fixer_add_blank_line (unused_error, errtext):
+    # E302 expected 2 blank lines, found 1
+    m = re.search(r"expected (\d+) blank lines, found (\d+)", errtext)
+    if m:
+        linecount = int(m.group(1)) - int(m.group(2))
+        lisp.beginning_of_line()
+        lisp.newline(linecount)
+
+
+def fixer_remove_blank_lines (unused_error, errtext):
+    # E303 too many blank lines (7)
+    linecount = 1
+    m = re.search(r"too many blank lines \((\d+)\)", errtext)
+    if m:
+        linecount = int(m.group(1))
+        # There's a bug in flake8 it's returning 2 lines too many
+        if linecount > 2:
+            linecount -= 2
+
+    # XXX try this with a comment right about the warning, deletes the comment
+    lisp.beginning_of_line()
+    beg = lisp.line_beginning_position()
+    lisp.beginning_of_line(-(linecount - 1))
+    lisp.forward_line(1)
+    lisp.forward_char(-1)
+    lisp.delete_region(beg, lisp.point())
+    return
+
+
+def fixer_remove_trailing_ws(unused_error, unused_text):
+    start, end, line = get_line_string()
+    m = re.match(r"^(.*\S+)?\s*$", line)
+    if not m:
+        #print("NO MATCH")
+        return
+    #print("MATCH")
+    if m.group(1):
+        newline = m.group(1)
+    else:
+        newline = ""
+    if newline != line:
+        lisp.delete_region(start, end)
+        lisp.insert(newline)
+
+
+def fixer_equals_none (unused_error, unused_errtext):
+    start, end, line = get_line_string()
+    newline = re.sub(r"==\s*None", r"is None", line)
+    newline = re.sub(r"!=\s*None", r"is not None", newline)
+    if newline != line:
+        lisp.delete_region(start, end)
+        lisp.insert(newline)
+
+
+def fixer_comma_space (unused_error, unused_errtext):
+    start, end, line = get_line_string()
+    newline = re.sub(r"\s*,\s*", r", ", line)
+    if newline != line:
+        lisp.delete_region(start, end)
+        lisp.insert(newline)
+
+
+def fixer_fixup_imports (error, errtext):
+    pass
+
+fixers = {
+    # Pyflake errors
+    "W291": fixer_remove_trailing_ws,  
+    "W293": fixer_remove_trailing_ws,
+    # "E225": fixer_add_one_space_around_equals,
+    # "E231": fixer_comma_space,
+    # "E251": fixer_remove_space_around_equals,
+    # "E301": fixer_add_blank_line,
+    # "E302": fixer_add_blank_line,
+    #"E303": fixer_remove_blank_lines,
+    "E401": fixer_fixup_imports,
+    "E711": fixer_equals_none,
+
+    # Pylint errors
+    # "W0511": fixer_xxx,
+}
+
+
+def get_fixer_func (errno):
+    "Return a function to call fo the given errno"
+    if errno in fixers:
+        return fixers[errno]
+    return None
+
+
+@make_lisp_interactive
+def fix_current_line ():
+    print("Got called")
+    lineno = lisp.flymake_current_line_no()
+    errinfo = lisp.flymake_err_info
+    errlist = lisp.nth(0, lisp.flymake_find_err_info(errinfo.value(), lineno))
+    menudata = lisp.flymake_make_err_menu_data(lineno, errlist)
+    did_something = False
+    #             caadr (x ((e1 e2 ...))) -> (e1 e2 ...)
+    for errtxt in menudata[1][0]:
+        if not errtxt:
+            break
+        m = re.match(r"([EFW]\d+) .*", errtxt)
+        if not m:
+            print("nomatch on {}".format(errtxt))
+            continue
+        key = m.group(1)
+        if key in fixers:
+            did_something = True
+            fixers[key](key, errtxt)
+    if did_something:
+        lisp.flymake_start_syntax_check()
+
+__author__ = 'Christian Hopps'
+__date__ = 'February 12 2014'
+__version__ = '1.0'
+__docformat__ = "restructuredtext en"
+
+# Local Variables:
+# pymacs-auto-reload: t
+# End:
