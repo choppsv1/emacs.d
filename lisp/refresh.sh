4bfa82df1c2a055da0dbee971b892b9e830fadb4
Christian E. Hopps <chopps@gmail.com>
2015-12-07 09:54:08 -0500

    fix C-tab and refresh script

--- lisp/refresh.sh
+++ lisp/refresh.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ -d ietf-doc ]; then
-    git pull git@github.com:choppsv1/theme-looper.git
-else
-    git clone git@github.com:choppsv1/theme-looper.git
-fi
+REPOS="mu4e-maildirs-extension theme-looper"
+
+for repo in $REPOS; do
+    if [ -d $repo ]; then
+        (cd $repo && git pull)
+    else
+        git clone git@github.com:choppsv1/$repo.git
+    fi
+done
